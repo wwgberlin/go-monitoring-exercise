07fe8ab4f19899c05c36bada749378266528f1c7
Ronna Steinberg <ronna.steinberg@daliaresearch.com>
2018-01-31 11:38:03 +0100

    task defs

--- main.go
+++ main.go
@@ -7,20 +7,32 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Task:
-// If you are here it means you already have prometheus installed
-// Add a promhttp.Handler() to the endpoint "/metrics/"
+
+// In order to let prometheus start consuming metrics from our app
+// we need to provide a /metrics/ endpoint.
+// The package github.com/prometheus/client_golang/prometheus/promhttp
+// already provides us with an http.Handler that we can use to do this.
+// Add the package to the imports and add call to
+// http.Handle("/metrics/", promhttp.Handler())
 // just before the call to ListenAndServe.
 // Checkout 127.0.0.1:8080/metrics.
-
 func main() {
 	reg := prometheus.DefaultRegisterer
 
-	go background(reg)
+	go func() {
+		for {
+			background(reg)
+			time.Sleep(1 * time.Second)
+		}
+	}()
 
 	newDemoAPI(reg).register(http.DefaultServeMux)
+
+	http.Handle("/metrics/", promhttp.Handler())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -30,7 +42,10 @@ type demoAPI struct {
 }
 
 // Task:
-// this function receives a registerer, create a prometheus counter vector
+
+// this function receives a prometheus Registerer.
+// create a prometheus Counter vector using prometheus.NewCounterVec:
+// with the name "background_task_results"
 // with the labels ["results"].
 // Where we encounter timeout, call vec.WithLabelValues with "timeout" and
 // Where we are successful, call WithLabelValues with "success".
@@ -42,37 +57,35 @@ type demoAPI struct {
 // grafana dashboard
 
 func background(registerer prometheus.Registerer) {
-	for {
-		ch := make(chan string)
-		go func() {
-			ch <- foo()
-		}()
-		select {
-		case str := <-ch:
-			log.Println(str)
-		case <-time.After(time.Millisecond * 50):
-			log.Println("gave up!")
-		}
-		time.Sleep(1 * time.Second)
+	ch := make(chan string)
+	go func() {
+		ch <- foo()
+	}()
+	select {
+	case str := <-ch:
+		log.Println(str)
+	case <-time.After(time.Millisecond * 50):
+		log.Println("gave up!")
 	}
 
 }
 
 // Task:
+
 // newDemoAPI is a constructor function that returns a pointer to a demoApi
 // The demoApi requires a histogram vector.
 // This is the histogram that will record our endpoints speed per endpoint.
-// Create the vector with the name endpoint_duration
-// Instantiate a new histogram vector
-// with the labels ["endpoint"].
+// Instantiate a new histogram vector (similarly to how we did for counters
+// with the name endpoint_duration and the labels ["endpoint"].
+// check out prometheus documentation to decide to configure your buckets
 // register the histogram to the registerer and return a demoApi object
 // with the histogram.
-
 func newDemoAPI(reg prometheus.Registerer) *demoAPI {
 	return &demoAPI{}
 }
 
 // Task:
+
 // The following function is missing some code.
 // inside this function we declared another anonymous function called instr
 // Wrap the call to fn(w,r) with timestamps to obtain the time it took to
